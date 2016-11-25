5f0b4766dcd36a69e3ca35683b51e4730d255b1f
carystokes <cary.p.stokes@gmail.com>
2016-11-24 22:09:49 -0500

    Portfolio Model clean-up

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,11 +2,5 @@ class Portfolio < ActiveRecord::Base
   belongs_to :user
   has_many :holdings, dependent: :destroy
 
-  def initialize(title, cash)
-    @title = title
-    @cash = cash
-    @holdings = self.holdings
-  end
-  
   validates :title, presence: true
 end
